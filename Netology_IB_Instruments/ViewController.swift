bf477a4e083d8102471fdb044e39133927ce5051
Rysya <pbl6a@bk.ru>
2025-11-10 12:40:03 +0300

    complete homework

--- Netology_IB_Instruments/ViewController.swift
+++ Netology_IB_Instruments/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var profileView: ProfileView!
+    
+    override func loadView() {
+        profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView
+        self.view = profileView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
